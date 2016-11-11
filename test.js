ac77195d5b088cbb9a979dc1501694c97a357fed
N0taN3rd <n0tan3rd@gmail.com>
2016-11-11 15:38:29 -0500

    um

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
 const Twit = require('twit')
 const fs = require('fs-extra')
 const twitter = Twit({
-  consumer_key: 'K1y1GmSdDfUmBNMJeX1lf8Ono',
-  consumer_secret: 'Ksd87lVkQWRVeXUIYjqqPF7mfUZuRq1aU1fgAFJHdDz3AY7NTY',
-  access_token: '4844579470-y1a1kQePvEohKDp8RDfESX1whNRhlTm856JHWn3',
-  access_token_secret: '46R2ynfMC8CmHzsd76UReneRGcPbuOaPAIhZVeMLKZD2f',
+
   timeout_ms: 60 * 1000,
 })
 
